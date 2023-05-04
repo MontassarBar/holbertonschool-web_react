78b011ac90fa85dc4b7cf61d9698937f480f2a6c
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-04 22:08:21 +0200

    task-2

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -16,13 +16,8 @@ module.exports = {
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          'file-loader',
           {
             loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
           },
         ],
       },
