6c80bba124f8470cec221bb8a26636def536f3a6
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-04 21:47:56 +0200

    task-2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,16 @@
+import $ from "jquery";
+import _ from 'lodash';
+
+$('<div id="logo"></div>').appendTo('body');
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</button>').appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
+
+let count = 0;
+function updateCounter() {
+count++;
+$('#count').text(`${count} clicks on the button`);
+}
+$('button').on('click', _.debounce(updateCounter, 500));
