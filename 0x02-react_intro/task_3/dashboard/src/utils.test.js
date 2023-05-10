10b262b6d2f2f0243a4d2c0988ceabf054f01c42
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-10 17:50:54 +0200

    task_3

---
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,18 @@
+const { getFullYear, getFooterCopy, getLatestNotification } = require('./utils');
+
+test("return the current year", () => {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toBe(currentYear);
+});
+
+test("When isIndex is true, the getFooterCopy function should return 'Holberton School'", () => {
+    expect(getFooterCopy(true)).toBe("Holberton School");
+});
+
+test("When isIndex is false, the getFooterCopy function should return 'Holberton School main dashboard'", () => {
+    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+});
+
+test("return '<strong>Urgent requirement</strong> - complete by EOD'" , () => {
+    expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD")
+});
