dd58a92e4abe8dd64685be7c768d0b2698495916
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-15 15:26:12 +0200

    task_0

---
+++ 0x03-react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Notifications.css'
+import closeIcon from '../assets/close-icon.png'
+import {getLatestNotification} from '../utils/utils'
+
+const Notifications = () => {
+    return (
+        <div className='Notifications'>
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority= 'default'>New course available</li>
+                <li data-priority= 'urgent'>New resume available</li>
+                <li data-priority= 'urgent' dangerouslySetInnerHTML={{ __html: `${getLatestNotification()}` }}></li>
+            </ul>
+            <button style={{
+                position: 'absolute',
+                right: '20px',
+                top: '20px',
+                background: 'transparent',
+                border: 'none',
+                cursor: 'pointer'}} aria-label="Close" onClick={() => console.log(`Close button has been clicked`)}>
+                    <img src={closeIcon} style={{width: '25px'}} alt="close icon"/>
+                </button>
+        </div>
+    )
+}
+
+export default Notifications;
