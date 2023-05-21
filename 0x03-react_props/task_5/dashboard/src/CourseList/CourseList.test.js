99f85a6a1f6e8ad87bf5e0bcf4734d65e65a799c
MontassarBar <montassarbarbouchi7@gmail.com>
2023-05-21 16:50:51 +0200

    tests

--- 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
+++ 0x03-react_props/task_5/dashboard/src/CourseList/CourseList.test.js
@@ -2,14 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CourseList from './CourseList';
 
+const listCourses = [
+    {id: 1, name: "ES6", credit: 60},
+    {id: 2, name: "Webpack", credit: 20},
+    {id: 3, name: "React", credit: 40}
+  ]
+
+const listCourses2 = []
+
 describe('<CourseList />', () => {
     it("Check that it renders CourseList component without crashing", () =>{
         shallow(<CourseList />);
     });
 
     it("Check that it renders the 5 different rows", () =>{
-        const wrapper = shallow(<CourseList />);
+        const wrapper = shallow(<CourseList listCourses={listCourses} />);
         expect(wrapper.find('CourseListRow').length).toBe(5);
+    });
+
+    it("verify that CourseList renders correctly if you pass an empty array", () =>{
+        const wrapper = shallow(<CourseList listCourses={listCourses2} />);
+        expect(wrapper.text().includes('No course available yet')).toBe(true);
+    });
+
+    it("verify that CourseList renders correctly if you pass an empty array", () =>{
+        const wrapper = shallow(<CourseList />);
+        expect(wrapper.text().includes('No course available yet')).toBe(true);
     });
 
 });
--- 0x03-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_5/dashboard/src/Notifications/Notifications.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Notifications from './Notifications';
+import { getLatestNotification } from '../utils/utils';
 
 const listNotifications = [
-    {id: 8, name: "ES6", credit: 60},
-    {id: 9, name: "Webpack", credit: 20},
-    {id: 10, name: "React", credit: 40}
+    {id: 1, type: "default", value: "New course available"},
+    {id: 2, type: "urgent", value: "New resume available"},
+    {id: 3, type: "urgent", __html: {__html: getLatestNotification()}},
   ]
+
+  const listNotifications2 = []
 
 
 describe('<Notifications />', () => {
@@ -15,17 +18,17 @@ describe('<Notifications />', () => {
     });
 
     it('verify that Notifications renders three list items', () => {
-        const wrapper = shallow(<Notifications displayDrawer={true}/>);
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
         expect(wrapper.find('NotificationItem').length).toBe(3);
     });
 
     it('verify that Notifications renders the text Here is the list of notifications', () => {
-        const wrapper = shallow(<Notifications displayDrawer={true}/>);
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
         expect(wrapper.text().includes('Here is the list of notifications')).toBe(true);
     });
 
     it('verify that the first NotificationItem element renders the right html', () => {
-        const wrapper = shallow(<Notifications displayDrawer={true}/>);
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
         const ul = wrapper.find('ul').childAt(0);
         expect(ul.html()).toBe('<li data-notification-type="default">New course available</li>');
     });
@@ -50,5 +53,13 @@ describe('<Notifications />', () => {
         expect(wrapper.find('div.Notifications').length).toBe(1);
     });
 
-    
+    it('verify that Notifications renders correctly if you pass an empty array', () => {
+        const wrapper = shallow(<Notifications displayDrawer={true} listNotifications={listNotifications2} />);
+        expect(wrapper.text().includes('No new notification for now')).toBe(true);
+    });
+
+    it("verify that Notifications renders correctly if you don't pass the listNotifications property", () => {
+        const wrapper = shallow(<Notifications displayDrawer={true} />);
+        expect(wrapper.text().includes('No new notification for now')).toBe(true);
+    });
 });
