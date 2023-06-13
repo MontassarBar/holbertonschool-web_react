900a2dc32f959bd7e3bffcde905fb2ad84287110
MontassarBar <montassarbarbouchi7@gmail.com>
2023-06-13 19:20:45 +0200

    Equality

--- 0x07-React_Immutable/7-equality.js
+++ 0x07-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
 import { Map, is } from 'immutable';
 
-export default function areMapsEqual(map1, map2) {
-  return is(Map(map1), Map(map2))
-}
+const areMapsEqual = (map1, map2) => is(Map(map1), Map(map2));
+
+export default areMapsEqual;
