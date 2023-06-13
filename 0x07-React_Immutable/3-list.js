b62e402d37146278af570d35cd7d28eefae7202b
MontassarBar <montassarbarbouchi7@gmail.com>
2023-06-13 17:40:43 +0200

    List and push

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,10 +1,10 @@
-import Immutable, { List } from 'immutable';
+import { List } from 'immutable';
 
 export function getListObject(array) {
-    return Immutable.List(array);
+    return List(array);
 }
 
-
 export function addElementToList(list, element) {
-   return list.push(element);
+    const list1 = List(list)
+    list1.push(element);
 }
