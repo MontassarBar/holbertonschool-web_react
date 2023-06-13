03d7f50b8ec5c40fe210547bac9232ee8d178e4f
MontassarBar <montassarbarbouchi7@gmail.com>
2023-06-13 17:17:55 +0200

    Accessing nested elements

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,8 +1,6 @@
-import { fromJS } from 'immutable';
+import { fromJS, getIn } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-    if (!object || !array || !array.length) return undefined;
-
-    const nested = fromJS(object);
-    return nested.getIn([...array]);
+  const map1 = fromJS(object);
+  return getIn(map1, array, undefined);
 }
